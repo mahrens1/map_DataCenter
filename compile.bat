fa68734502f0acf1f24c6035c162030a8a9c58d4
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-10-15 13:10:54 -0500

    Moved folder

--- compile.bat
+++ compile.bat
@@ -1,29 +1,29 @@
 @echo off
 
 cd\
-cd F:\CCC Fall 2020\Game Design 2\map_DataCenter\map_DataCenter
+cd F:\map_DataCenter
 
 
 echo Copying Files...
-copy F:\CCC Fall 2020\Game Design 2\map_DataCenter\map_DataCenter\id1\maps\DataCenter.map F:\CCC Fall 2020\Game Design 2\map_DataCenter\map_DataCenter
+copy F:\map_DataCenter\id1\maps\DataCenter.map F:\map_DataCenter
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-F:\CCC Fall 2020\Game Design 2\map_DataCenter\map_DataCenter\tools\ericw-tools\bin\qbsp.exe DataCenter
+F:\map_DataCenter\tools\ericw-tools\bin\qbsp.exe DataCenter
 
 echo --------------VIS---------------
-F:\CCC Fall 2020\Game Design 2\map_DataCenter\map_DataCenter\tools\ericw-tools\bin\vis.exe DataCenter
+F:\map_DataCenter\tools\ericw-tools\bin\vis.exe DataCenter
 
 echo -------------LIGHT--------------
-F:\CCC Fall 2020\Game Design 2\map_DataCenter\map_DataCenter\tools\ericw-tools\bin\light.exe DataCenter
+F:\map_DataCenter\tools\ericw-tools\bin\light.exe DataCenter
 
-copy DataCenter.bsp F:\CCC Fall 2020\Game Design 2\map_DataCenter\map_DataCenter\id1\maps
-copy DataCenter.pts F:\CCC Fall 2020\Game Design 2\map_DataCenter\map_DataCenter\id1\maps
-copy DataCenter.lit F:\CCC Fall 2020\Game Design 2\map_DataCenter\map_DataCenter\id1\maps
+copy DataCenter.bsp F:\map_DataCenter\id1\maps
+copy DataCenter.pts F:\map_DataCenter\id1\maps
+copy DataCenter.lit F:\map_DataCenter\id1\maps
 pause
 cd\
-cd F:\CCC Fall 2020\Game Design 2\map_DataCenter\map_DataCenter
+cd F:\map_DataCenter
 quakespasm  +map DataCenter
